a0d8efd99cca06ce41746b2aa61c2037f2d536a1
artemis-env0 <artem.artyunov@env0.com>
2025-11-03 09:36:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ terraform {
 }
 
 output "hello" {
-  value = "From env0 gke Connectivity Test 🐍 If you see this that means you're connected to Google Public Cloud and Test was Successful"
+  value = "From env0 gke Connectivity Test 🚀 If you see this that means you're connected to Google Public Cloud and Test was Successful"
 }
