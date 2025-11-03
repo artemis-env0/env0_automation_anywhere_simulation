f49ab2f987e8c82a238030f4d0c868610f01ba9b
artemis-env0 <artem.artyunov@env0.com>
2025-11-03 09:23:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,4 @@ MVT - (main.tf)
 terraform {
   required_version = ">= 1.4.0"
 }
-output "hello there, if you see this that means the environment is connected and everything is working as it should" { value = "env0 gke connectivity test" }
+output "hello" { value = "env0 gke connectivity test" }
